077ad673e385222afbe9cfe21b7cc9b42405f185
Srikanth <aluvalasrikanth35@gmail.com>
2025-08-22 15:14:57 +0530

    Add files via upload

---
+++ Strings/AnagramInstring.java
@@ -0,0 +1,24 @@
+
+// write a program fo Anagram is or not
+
+import java.util.Arrays;
+class AnagramInstring
+{
+	public static void main(String [] args)
+	{
+		String s1="listen";
+		String s2="silent";
+		char a1[]=s1.toCharArray();
+		char a2[]=s2.toCharArray();
+		Arrays.sort(a1);
+		Arrays.sort(a2);
+	if(Arrays.equals(a1,a2))
+		{
+		    System.out.println("is anagram");
+		}
+		else
+		{
+		   System.out.println("is not anagram"); 
+		}
+	}
+}
