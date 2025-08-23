5ee0e8f7414b09dfc1f23008d9af8c15e36eb37d
Srikanth <aluvalasrikanth35@gmail.com>
2025-08-23 23:03:20 +0530

    Add files via upload

---
+++ Basic_Programs/StudentMarks.java
@@ -0,0 +1,25 @@
+class marks
+{
+	public static void main(String [] args)
+	{
+		int marks=77;
+		
+		 if(marks>=90)
+		 {
+			 System.out.println("Grade A");
+		 }
+		else if( marks>=70)
+		{ 
+                        System.out.print("Grade B");
+		 }
+		else if( marks>=40)
+		 { 
+                        System.out.print("Grade C");
+	         }	
+                  else
+	          { 
+                       System.out.print("Fail");
+		  }			 
+	    
+	}
+}
